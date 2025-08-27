26edef9a3020be529b9401c461fd8c5f422f3ee8
theGiorgio22 <jf61530622@gmail.com>
2025-08-27 12:47:13 -0400

    Update dAlumno.php

--- datos/dAlumno.php
+++ datos/dAlumno.php
@@ -1,14 +1,13 @@
 <?php
-include './dConexion.php';
+include_once 'dConexion.php';
+
 class dAlumno {
-    //put your code here
     private $codigo;
     private $nombre;
     private $apellido;
     private $direccion;
     private $telefono;
     
-    //construct
     function __construct($cod, $nom, $ape, $dir, $tel) {
         $this->codigo = $cod;
         $this->nombre = $nom;
@@ -16,26 +15,28 @@ class dAlumno {
         $this->direccion = $dir;
         $this->telefono = $tel;
     }
+
     function Adicionar() {
-        $cone= new dConexion();
-        $respuesta=true;
+        $cone = new dConexion();
+        $con = $cone->Conectar();
+        $respuesta = false;
         try {
-            $con=$cone->Conectar();
             mysqli_begin_transaction($con);
-            $consulta="insert into alumno(codigo,nombre,apellido,direccion) values "."($this->codigo,$this->nombre,$this->apellido,$this->direccion,$this->telefono);";
-            $resultado= mysqli_query($con, $consulta);
+            $consulta = "INSERT INTO alumno (codigo, nombre, apellido, direccion, telefono) 
+                         VALUES ('$this->codigo', '$this->nombre', '$this->apellido', '$this->direccion', '$this->telefono')";
+            $resultado = mysqli_query($con, $consulta);
+            
             if ($resultado) {
-                $respuesta= mysqli_rollback($con);
+                mysqli_commit($con);
+                $respuesta = true;
             } else {
-                $respuesta= mysqli_commit($con);
+                mysqli_rollback($con);
             }
             mysqli_close($con);
         } catch (Exception $ex) {
-            try{
-                mysqli_close($con);
-            } catch (Exception $ex) {
-
-            }
+            mysqli_rollback($con);
+            mysqli_close($con);
         }
+        return $respuesta;
     }
 }
