b977aa878a0e6e53ad8862d35a1bacc0620e6703
theGiorgio22 <jf61530622@gmail.com>
2025-08-27 12:47:32 -0400

    Update dConexion.php

--- datos/dConexion.php
+++ datos/dConexion.php
@@ -1,45 +1,31 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of dConexion
- *
- * @author Usuario
- */
 class dConexion {
-    //put your code here
     private $servidor;
     private $basededatos;
     private $puerto;
     private $usuario;
     private $clave;
-    //construc..
-    function __construct($servidor, $basededatos, $puerto, $usuario, $clave) {
+
+    function __construct() {
         $this->servidor = "localhost";
-        $this->basededatos = "bdregistronotas";
-        $this->puerto = 3308;
+        $this->basededatos = "bdsistemaregistronotas";
+        $this->puerto = 3306;
         $this->usuario = "root";
         $this->clave = "";
     }
-    function Conectar() {
-        try {
-            $con = mysqli_connect($this->servidor, $this->usuario, $this->clave, $this->basededatos, $this->puerto);
 
-            if (!$con) {
-                echo 'Error en la conexión a la base de datos';
-                return null;
-            } else {
-                // echo 'Conexión exitosa';
-            }
-        } catch (Exception $e) {
-            echo "Error: " . $e->getMessage();
-            return null;
+    function Conectar() {
+        $con = mysqli_connect(
+            $this->servidor,
+            $this->usuario,
+            $this->clave,
+            $this->basededatos,
+            $this->puerto
+        );
+
+        if (!$con) {
+            die("Error en la conexión a la base de datos: " . mysqli_connect_error());
         }
-        return $con; //Retorna una conexion
+        return $con;
     }
 }
