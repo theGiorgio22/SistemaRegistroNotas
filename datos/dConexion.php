0b840c4853289c8a7156f6eced5f854f452abc0e
theGiorgio22 <jf61530622@gmail.com>
2025-09-04 19:20:32 -0400

    Update dConexion.php

--- datos/dConexion.php
+++ datos/dConexion.php
@@ -1,31 +1,17 @@
 <?php
 class dConexion {
-    private $servidor;
-    private $basededatos;
-    private $puerto;
-    private $usuario;
-    private $clave;
-
-    function __construct() {
-        $this->servidor = "localhost";
-        $this->basededatos = "bdsistemaregistronotas";
-        $this->puerto = 3306;
-        $this->usuario = "root";
-        $this->clave = "";
-    }
+    private $servidor = "localhost";
+    private $usuario = "root";
+    private $clave = "";
+    private $bd = "bdsistemaregistronotas";
+    private $puerto = 3308;
 
     function Conectar() {
-        $con = mysqli_connect(
-            $this->servidor,
-            $this->usuario,
-            $this->clave,
-            $this->basededatos,
-            $this->puerto
-        );
-
+        $con = mysqli_connect($this->servidor, $this->usuario, $this->clave, $this->bd, $this->puerto);
         if (!$con) {
-            die("Error en la conexión a la base de datos: " . mysqli_connect_error());
+            die("❌ Error de conexión a la base de datos: " . mysqli_connect_error());
         }
         return $con;
     }
 }
+?>
